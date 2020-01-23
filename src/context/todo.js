2476ee6e3d811bff9c1736d51b3d7cd759a8698d
edumoreira1506 <contato@eduardoem.com.br>
2020-01-23 10:40:46 -0300

    Create model for todo

--- src/context/todo.js
+++ src/context/todo.js
@@ -1,13 +1,14 @@
 import React, { useState, createContext } from 'react';
+import * as Todo from '../models/Todo';
 
 export const TodoContext = createContext()
 
 export const TodoProvider = ({ children }) => {
   const [todos, setTodos] = useState([]);
 
-  const addTodo = todo => setTodos([...todos, todo]);
+  const addTodo = todo => setTodos(Todo.addTodo(todos, todo));
 
-  const removeTodo = todo => setTodos(todos.filter(item => item !== todo));
+  const removeTodo = todo => setTodos(Todo.removeTodo(todos, todo));
 
   return (
     <TodoContext.Provider value={[todos, addTodo, removeTodo]}>
